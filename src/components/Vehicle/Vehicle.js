88af7fb3c3dee581bda01568edf6ff83ecca3812
L J <laprje@gmail.com>
2019-11-25 16:22:18 -0700

    added chartjs functionality

--- src/components/Vehicle/Vehicle.js
+++ src/components/Vehicle/Vehicle.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Header from "../Header/Header";
 import { connect } from "react-redux";
 import axios from "axios";
+import { Line } from "react-chartjs-2";
+import './Vehicle.css'
 
 class Vehicle extends Component {
   constructor() {
@@ -12,17 +14,60 @@ class Vehicle extends Component {
         model: "",
         year: "",
         img: ""
+      },
+      data: {
+        labels: [],
+        datasets: [
+          {
+            label: "Value ($USD)",
+            data: [],
+            backgroundColor: ["rgb(106, 226, 160)"]
+          }
+        ]
       }
     };
   }
 
   getCar() {
     const { make, model, year } = this.props;
-    axios.post("/api/car", { make, model, year }).then(res => {
-      this.setState({
-        carOnDisplay: res.data
+    axios
+      .get(
+        `https://www.trueavm.com/trueavm/autoValue.do?make=${make}&model=${model}&year=${year}&count=5&key=85ut2hrj7ps4u8xwhv64`
+      )
+      .then(res => {
+
+     
+
+        let key = 'data'
+        
+
+        const arr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+
+        this.setState(prevState => ({
+          data: {
+            ...prevState.data,
+            datasets: [
+              {
+              label: "Value ($USD)",
+              data: res.data.map(el => el["value"].split('').filter((el) => arr.includes(el)).join('')),
+              backgroundColor: ["rgb(106, 226, 160)"]
+            }
+          ]
+          }}),()=>console.log(this.state.data.datasets[0])
+          
+          )
+
+        this.setState(
+          prevState => ({
+            data: {
+              ...prevState.data,
+              labels: res.data.map(el => {
+                return el.date;
+              })
+            }
+          })
+        );
       });
-    });
   }
 
   componentDidMount() {
@@ -30,22 +75,29 @@ class Vehicle extends Component {
   }
 
   render() {
-
-
-    const { carOnDisplay } = this.state;
     return (
       <div className="vehicle">
         <Header />
-        {carOnDisplay.length === 1 ? (
-          <div>
-            <h3>{this.state.carOnDisplay[0].year}</h3>
-            <h3>{this.state.carOnDisplay[0].make}</h3>
-            <h3>{this.state.carOnDisplay[0].model}</h3>
-            <img src={this.state.carOnDisplay[0].img} alt="" />
+        {this.props.make && this.props.model && this.props.year ? (
+          <div className="car-info">
+            <h3>{this.props.year}</h3>
+            <h3>{this.props.make}</h3>
+            <h3>{this.props.model}</h3>
+            {/* <img src={this.state.carOnDisplay[0].img} alt="" /> */}
           </div>
         ) : (
           <p>Loading...</p>
         )}
+        <div className="chart-container">
+          <Line 
+          data={this.state.data} options={{
+            title:{
+              display: true,
+              text: `Your ${this.props.year} ${this.props.make} ${this.props.model}'s expected value over the next 5 years`
+            }
+          }} 
+          />
+        </div>
       </div>
     );
   }
--- src/components/Wizard/Wizard1.js
+++ src/components/Wizard/Wizard1.js
@@ -2,43 +2,125 @@ import React, { Component } from "react";
 import Header from "../Header/Header";
 import { connect } from "react-redux";
 import { updateMake, updateModel, updateYear } from "../../ducks/reducer";
-import './Wizard.css'
-import axios from 'axios'
+import "./Wizard.css";
+import axios from "axios";
 
 class Wizard1 extends Component {
-
   constructor() {
-    super()
+    super();
     this.state = {
-      makes: ''
-    }
+      makes: [],
+      chosenMake: "",
+      models: [],
+      chosenModel: "",
+      years: [
+        2020,
+        2019,
+        2018,
+        2017,
+        2016,
+        2015,
+        2014,
+        2013,
+        2012,
+        2011,
+        2010,
+        2009,
+        2008,
+        2007,
+        2006,
+        2005,
+        2004,
+        2003,
+        2002,
+        2001,
+        2000,
+        1999,
+        1998,
+        1997,
+        1996,
+        1995,
+        1994,
+        1993,
+        1992,
+        1991,
+        1990
+      ],
+      chosenYear: ""
+    };
+    this.getModels = this.getModels.bind(this);
   }
 
-  handleChange = (key, value) => {
-    this.setState({ [key]: value });
-  };
+  componentDidMount() {
+    axios
+      .get(
+        "https://www.trueavm.com/trueavm/autoMakes.do?key=85ut2hrj7ps4u8xwhv64"
+      )
+      .then(res => {
+        this.setState({ makes: res.data });
+      });
+  }
+
+  getModels() {
+    axios
+      .get(
+        `https://www.trueavm.com/trueavm/autoModels.do?make=${this.state.chosenMake}&key=85ut2hrj7ps4u8xwhv64`
+      )
+      .then(res => {
+        this.setState({ models: res.data });
+      });
+  }
 
   render() {
     const { updateMake, updateModel, updateYear } = this.props;
-   
-    // axios.get('https://www.trueavm.com/trueavm/autoMakes.do?key=85ut2hrj7ps4u8xwhv64').then(res => {
-    //    this.setState({makes: res.data})
-    //    console.log(this.state.makes)
-    // })
-    
+
     return (
       <div className="wizard">
         <Header />
         <div className="container">
-        <h2 className="wizard-header">Find Your Car</h2>
-        <div className="inputs">
-          {/* <select className="make" id="">
-            {}
-          </select>
-          <select className="model" id=""></select>
-          <select className="year" id=""></select> */}
-          {/* // inputs from before adding dropdown // */}
-           <input
+          <h2 className="wizard-header">Find Your Car</h2>
+          <div className="inputs">
+            <select
+              className="make"
+              name="make"
+              value={this.state.chosenMake}
+              onChange={e => {
+                this.setState({ chosenMake: e.target.value }, () => {
+                  this.getModels();
+                });
+                updateMake(e.target.value);
+              }}
+            >
+              <option>Select a Vehicle Make</option>
+              {this.state.makes.map(el => {
+                return <option value={el.value}>{el}</option>;
+              })}
+            </select>
+            <select
+              className="model"
+              name="model"
+              onChange={e => {
+                this.setState({ chosenModel: e.target.value });
+                updateModel(e.target.value);
+              }}
+            >
+              <option>Select a Model</option>
+              {this.state.models.map(el => {
+                return <option value={el.value}>{el}</option>;
+              })}
+            </select>
+            <select className="year" name="year" onChange={e => {
+              this.setState({ chosenYear: e.target.value})
+              updateYear(e.target.value)
+              }}>
+              <option>Select A Year</option>
+              {this.state.years.map(el => {
+                return <option value={el.value}>{el}</option>;
+              })}
+            </select>
+
+            {/* // inputs from before adding dropdown // */}
+            {/* <input
             placeholder={
               this.props.make ? this.props.make : "Enter a Vehicle Make"
             }
@@ -58,22 +140,21 @@ class Wizard1 extends Component {
             }
             type="text"
             onChange={e => updateYear(e.target.value)}
-          />
-
-        </div>
-        <div className="buttons">
-          <button
-            onClick={() => this.props.history.push("/home")}
-            className="back-button"
-          >
-            Cancel
-          </button>
-          <button
-            onClick={() => this.props.history.push("/vehicle")}
-            className="next-button"
-          >
-            Search
-          </button>
+          /> */}
+          </div>
+          <div className="buttons">
+            <button
+              onClick={() => this.props.history.push("/home")}
+              className="back-button"
+            >
+              Cancel
+            </button>
+            <button
+              onClick={() => this.props.history.push("/vehicle")}
+              className="next-button"
+            >
+              Search
+            </button>
           </div>
         </div>
       </div>
@@ -84,14 +165,14 @@ class Wizard1 extends Component {
 function mapStateToProps(reduxState) {
   const { make, model, year } = reduxState;
   return {
-    make, model, year
+    make,
+    model,
+    year
   };
 }
 
-export default connect(mapStateToProps, { updateMake, updateModel, updateYear })(Wizard1);
-
-
-
-
-
-
+export default connect(mapStateToProps, {
+  updateMake,
+  updateModel,
+  updateYear
+})(Wizard1);
