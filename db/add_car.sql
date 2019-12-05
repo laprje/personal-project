298935c44de740dadd7b90865b9d1bf7e5c7a1ed
L J <laprje@gmail.com>
2019-12-05 15:34:17 -0700

    fixed saved cars bug

--- db/add_car.sql
+++ db/add_car.sql
@@ -1,4 +1,4 @@
 UPDATE users 
-SET cars = concat('{"make": "', $1, '", "model": "', $2, '", "year": "', $3, '"}', $4);
-
+SET cars = concat('{"make": "', $1, '", "model": "', $2, '", "year": "', $3, '"}')
+WHERE email = $4;
 -- "{\"make\": $1, \"model\": $2, \"year\": $3}"
