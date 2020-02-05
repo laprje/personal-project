52b92fbdf8ddd615d1e23327d3b5cf1f9c411461
L J <laprje@gmail.com>
2020-02-04 19:21:22 -0700

    css animations and card specs

--- src/components/ReleaseCard/ReleaseCard.js
+++ src/components/ReleaseCard/ReleaseCard.js
@@ -3,12 +3,28 @@ import axios from "axios";
 import "./ReleaseCard.css";
 import Swal from "sweetalert2";
 
-export default function ReleaseCard () {
-    return(
-        <div className="release-card">
-            <div className="card-info">
-
+export default function ReleaseCard(props) {
+  return (
+    <div className="release-card">
+      <div className="card-info">
+        <div className="img-container">
+          <img
+            className="car-image"
+            src={props.img ? props.img : "assets/img-not-found.png"}
+            alt="car image"
+          />
+        </div>
+        <div className="right-box">
+            <h2>{props.year?props.year:'2069'} {props.make?props.make:'vOlVo'} {props.model?props.model:'V60'}</h2>
+            <div className="info">
+                <h6>Release Date: {props.releaseDate?props.releaseDate:'N/A'}</h6>
+                <h6>Price: {props.price?"$"+props.price:'N/A'}</h6>
+                <h6>Engine Type: {props.engineType?props.engineType:"N/A"}</h6>
+                
             </div>
         </div>
-    )
+        <button className="expand"><i class="fas fa-chevron-right"></i></button>
+      </div>
+    </div>
+  );
 }
