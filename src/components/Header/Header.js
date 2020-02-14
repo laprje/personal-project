fcb179c4f7c3c893e4e536bbe524db16af63e39d
L J <laprje@gmail.com>
2020-02-13 19:23:07 -0700

    tried hover hints

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -43,7 +43,7 @@ class Header extends Component {
           <div className="button-container">
             <Link className="username" to="/profile">
               <div className="profile-link" id="greeting">
-                <h3 >Hello, {this.props.email}</h3>
+                <h3>Hello, {this.props.email}</h3>
                 <button className="icon">
                   <i className="fas fa-user"></i>
                 </button>
